f6560cb412e60fe3bcdf047f67614b0cee5cde7f
zhuming <15255189959@163.com>
2015-02-08 05:05:21 +0800

    linking pages

--- application_ming.py
+++ application_ming.py
@@ -3,6 +3,7 @@
 from flask import Flask,render_template,request
 
 app_ming = Flask(__name__)
+app_ming.vars = {}
 
 @app_ming.route('/index_ming',methods = ['GET','POST'])
 def index_ming():
@@ -10,7 +11,15 @@ def index_ming():
 	if request.method == 'GET':
 		return render_template('userinfo_ming.html',num = nquestions)
 	else:
-		return 'request.method was not a GET!'
+		app_ming.vars['name'] = request.form['name_ming']
+		app_ming.vars['age'] = request.form['age_ming']
+
+		f = open('%s_%s.txt'%(app_ming.vars['name'],app_ming.vars['age']),'w')
+		f.write('Name:%s\n'%(app_ming.vars['name']))
+		f.write('Age:%s\n\n'%(app_ming.vars['age']))
+		f.close()
+
+		return render_template('layout_ming.html',num=1,question='How many eyes do you have?',ans1='1',ans2='2',ans3='3')
 
 if __name__ == "__main__":
 	app_ming.run(debug=True)
