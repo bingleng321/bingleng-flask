5f4d19ac4ea02d1b2671fc6d41e0b9d103d67cb2
zhuming <15255189959@163.com>
2015-02-08 03:44:28 +0800

    all about requests which I have known

--- application_ming.py
+++ application_ming.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app_ming = Flask(__name__)
 
-@app_ming.route('/index_ming')
+@app_ming.route('/index_ming',methods = ['GET','POST'])
 def index_ming():
 	nquestions = 5
-	return render_template('userinfo_ming.html',num = nquestions)
+	if request.method == 'GET':
+		return render_template('userinfo_ming.html',num = nquestions)
+	else:
+		return 'request.method was not a GET!'
 
 if __name__ == "__main__":
 	app_ming.run(debug=True)
