be2367c4bf107592e44d8f9599b9d7de34f159eb
zhuming <15255189959@163.com>
2015-02-08 05:43:16 +0800

    linking to next question

--- application_ming.py
+++ application_ming.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 
 app_ming = Flask(__name__)
 app_ming.vars = {}
+
+@app_ming.route('/next_ming',methods=['POST'])
+def next_ming():
+	return redirect('/usefulfunction_ming')
+
+@app_ming.route('/usefulfunction_ming',methods=['GET','POST'])
+def usefulfunction_ming():	
+	return render_template('layout_ming.html',num=1,question='which fruit do you like \
+			best?',ans1='apple',ans2='banana',ans3='orange')
 
 @app_ming.route('/index_ming',methods = ['GET','POST'])
 def index_ming():
