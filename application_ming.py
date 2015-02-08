c861039c5ef81d4f3f158878b9ce0e781f5815d5
zhuming <15255189959@163.com>
2015-02-08 18:32:52 +0800

    grand finale:GET & POST or linking next page or end page

--- application_ming.py
+++ application_ming.py
@@ -5,18 +5,16 @@ from flask import Flask,render_template,request,redirect
 app_ming = Flask(__name__)
 app_ming.vars = {}
 
-@app_ming.route('/next_ming',methods=['POST'])
-def next_ming():
-	return redirect('/usefulfunction_ming')
+app_ming.questions = {}
+app_ming.questions['How many eyes do you have?'] = ('1','2','3')
+app_ming.questions['Which fruit do you like best?'] = ('banana','apple','orange')
+app_ming.questions['Do you like cupcakes'] = ('Yes','No','Maybe')
 
-@app_ming.route('/usefulfunction_ming',methods=['GET','POST'])
-def usefulfunction_ming():	
-	return render_template('layout_ming.html',num=1,question='which fruit do you like \
-			best?',ans1='apple',ans2='banana',ans3='orange')
+app_ming.nquestions = len(app_ming.questions)
 
 @app_ming.route('/index_ming',methods = ['GET','POST'])
 def index_ming():
-	nquestions = 5
+	nquestions = app_ming.nquestions
 	if request.method == 'GET':
 		return render_template('userinfo_ming.html',num = nquestions)
 	else:
@@ -27,8 +25,35 @@ def index_ming():
 		f.write('Name:%s\n'%(app_ming.vars['name']))
 		f.write('Age:%s\n\n'%(app_ming.vars['age']))
 		f.close()
+		return redirect('/main_ming')
 
-		return render_template('layout_ming.html',num=1,question='How many eyes do you have?',ans1='1',ans2='2',ans3='3')
+
+@app_ming.route('/main_ming')
+def main_ming2():
+	if len(app_ming.questions) == 0:
+		return render_template('end_ming.html')
+	else:
+		return redirect('/next_ming')
+
+
+@app_ming.route('/next_ming',methods=['GET','POST'])
+def next_ming():
+	if request.method == 'GET':
+		n = app_ming.nquestions - len(app_ming.questions) + 1
+		q = app_ming.questions.keys()[0]
+		a1,a2,a3 = app_ming.questions.values()[0]
+
+		app_ming.currentq = q
+		return render_template('layout_ming.html',num = n,question = q,\
+				ans1 = a1, ans2 = a2, ans3 = a3)
+	else:
+		f = open('%s_%s.txt'%(app_ming.vars['name'],app_ming.vars['age']),'a')
+		f.write('%s\n'%(app_ming.currentq))
+		f.write('%s\n\n'%(request.form['answer_form_layout_ming']))
+		f.close()
+
+		del app_ming.questions[app_ming.currentq]
+		return redirect('/main_ming')
 
 if __name__ == "__main__":
 	app_ming.run(debug=True)
